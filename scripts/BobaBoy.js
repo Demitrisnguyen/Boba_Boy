2d953f13f801e7da8774bf28acac2056ca2caa9d
Demitrisnguyen <75496496+Demitrisnguyen@users.noreply.github.com>
2020-12-10 10:25:40 -0800

    I made some changes

--- scripts/BobaBoy.js
+++ scripts/BobaBoy.js
@@ -1,12 +1,23 @@
 BobaBoy = {
 
+    container: document.getElementById("Boba_container"),
+
     init: function() {
+      this.createBobaBoy()
     },
     
     createBobaBoy: function(){
       let bobaboydiv = document.createElement("div");
       bobaboydiv.classname = "boy"
-      this.Boba_container.append(bobaboydiv)
+      this.container.append(bobaboydiv)
+      let BobaBoy = {
+        color: "black",
+        radius: 10,
+        x_pos: Math.random() * 700,
+        y_pos: Math.random() * 500,
+        x_vel: 0,
+        y_vel: 0,
+      }
     },
 
     createObstacles: function(){
