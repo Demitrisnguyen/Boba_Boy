95a389e00bf4271bfc8fa417619711347e9c33ac
Demitrisnguyen <75496496+Demitrisnguyen@users.noreply.github.com>
2020-12-10 09:44:51 -0800

    I made the  function stumps

--- scripts/BobaBoy.js
+++ scripts/BobaBoy.js
@@ -1,8 +1,56 @@
 BobaBoy = {
 
     init: function() {
-    }
-  
+    },
+    
+    createBobaBoy: function(){
+      //bruh
+    },
+
+    createObstacles: function(){
+
+    },
+
+    createHearts: function(){
+
+    },
+
+    createBoba: function(){
+
+    },
+
+    createPlatforms: function(){
+
+    },
+
+    animateBobaBoy: function(){
+
+    },
+
+    startLevel: function(){
+
+    },
+
+    pause: function(){
+
+    },
+
+    resume: function(){
+
+    },
+
+    collision: function(){
+      //decide if this is going to be the player checking for collisions with objects
+      // or the objects checking for collisions with the player.
+    },
+
+    jump: function(){
+
+    },
+
+    walk: function(){
+
+    },
   }
   
 BobaBoy.init();
