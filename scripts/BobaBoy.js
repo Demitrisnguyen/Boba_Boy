951b5a1cf798dd50c367fb841f602f675d232621
Demitrisnguyen <75496496+Demitrisnguyen@users.noreply.github.com>
2020-12-11 10:19:48 -0800

    I made some cahnges to the movebobaboy function

--- scripts/BobaBoy.js
+++ scripts/BobaBoy.js
@@ -75,15 +75,16 @@ BobaBoyApp = {
     },
 
     walk: function(){
-
+    
     },
 
     moveBobaBoy: function(){
       this.Boy.x_pos = this.Boy.x_pos + this.Boy.x_vel
       this.Boy.y_pos = this.Boy.y_pos - this.Boy.y_vel
-
-      this.Boy.element.style.left = this.Boy.x_pos
-      this.Boy.element.style.top = this.Boy.y_pos
+      
+//these go in a render function:
+      //this.Boy.element.style.left = this.Boy.x_pos
+      //this.Boy.element.style.top = this.Boy.y_pos
     },
   }
   
